be6e0713bc0d536fbc4a4c49742e6d15c2747c88
YoLoveLife <Dski_miKu@msn.com>
2016-12-09 16:14:50 +0800

    sys.path mod

--- test/testlogin.py
+++ test/testlogin.py
@@ -1,20 +1,20 @@
 #!/usr/local/python/bin/python
 # Time 2016-12-9 14:51
 # Author Yo
-
+import os,sys
+parent_path=os.path.dirname(sys.path[0])
+if parent_path not in sys.path:
+    sys.path.append(parent_path)
 import HttpLogin.login as login
 import HttpLogin.cookie as cookie
-
 URL="http://192.168.208.130/index.php"
 USERNAME='Admin'
 USERPASSWD='zabbix'
-
+FLAG=sys.argv[1]
 print("Cookie 获取")
 cookiee=cookie.GetCookie(URL)
-print(cookiee)
 HTML=login.PostLogin(cookiee,USERNAME,USERPASSWD)
-print(HTML)
-if 'Dashboard' in HTML.decode():
+if FLAG in HTML.decode():
     print("App Online")
 else:
     print("App Offline")
